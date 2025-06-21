3ae05e206fdc442732679a347d85e055bdb2bc69
Psaivenkat <psvenkat3113@gmail.com>
2025-06-21 22:56:13 +0530

    Reconciliation algorithm(React Fiber), React Hooks

--- README.md
+++ README.md
@@ -5,4 +5,13 @@
 - Dev Build
 - local server
 - HMR (Hot Module Replacement)---> refreshing
-- 
+- Filee watching algorithm
+- Catching - Faster Builds
+- Image optimization
+- minification
+- Bundling
+- compressing
+- code splitting
+- diffferential bundling ---> suport older browsers
+- Error handling
+- Https
--- src/components/Body.js
+++ src/components/Body.js
@@ -1,14 +1,40 @@
 import RestaurantCard from "./RestaurantCard";
 import resList from "../utils/mockData";
+import { useState } from "react";
+
 const Body = () =>{
+
+    // State variable - super powerful variable
+     const [listOfRestaurants, setListOfRestaurants] = useState(resList);
+    // const [searchText, setSearchText] = useState("");
+    
+
+
+
     return(
         <div className="body">
             <div className="search">
                 <input type="text" placeholder="Search for food items" />
                 <button className="search-btn">Search</button>
             </div>
+
+            <div className="filter">
+                    <button 
+                    className="filter-btn"
+                    onClick={() => {
+
+                        const filteredList = listOfRestaurants.filter(
+                            (restaurant) => restaurant.card.card.info.avgRating > 4
+                        );
+                        setListOfRestaurants(filteredList);
+
+                    }}
+                    >
+                    Top rated restaurants
+                    </button>
+            </div>
             <div className="res-container">
-                { resList.map((restaurant) => (
+                { listOfRestaurants.map((restaurant) => (
                     <RestaurantCard 
                         key={restaurant.card.card.info.id} 
                         resData={ restaurant} 
@@ -18,5 +44,7 @@ const Body = () =>{
         </div>
     );
 };
+
+
 
 export default Body;
