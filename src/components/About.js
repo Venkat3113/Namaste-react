bbc548ccf41928300cb6901c7e96f7a27a8de62b
Psaivenkat <psvenkat3113@gmail.com>
2025-06-26 12:37:58 +0530

    Class Based Components Using GITHUB USER API

--- src/components/About.js
+++ src/components/About.js
@@ -6,8 +6,8 @@ const About = ()=>{
         <div className="about">
             <h1>About Us</h1>
             <p>This is Namaste - React web series - About us page </p>
-            <User name={"Venkat(Function)"} location="vizianagaram" gmail="psvenkat3119@gmail.com"/>
-            <UserClass name={"Venkat(class)"} location="vizianagaram" gmail="psvenkat3119@gmail.com" />
+
+            <UserClass />
         </div>
     )
 
--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -3,18 +3,38 @@ import react from 'react';
 class UserClass extends react.Component {
     constructor(props){
         super(props);
+
+        this.state={
+           userInfo:{
+            name: "Dummy Name",
+            location: "Dummy Location",
+           },
+        };
+    }
+
+    async componentDidMount() {
+
+        const data = await fetch("https://api.github.com/users/Venkat3113");
+        const json = await data.json();
+        
+        this.setState({
+                userInfo:json,
+                        
+        }); 
+
+
     }
 
     render(){
-        const{name, location, gmail} = this.props;
+        const{name, location, email, avatar_url} = this.state.userInfo;
         return(
          
             
-
             <div className="user-card">
+            <img src={avatar_url} />
             <h2>Name : {name}</h2>
             <h3>Location : {location}</h3>
-            <h4>Contact : {gmail}</h4>    
+            <h4>Contact : {email}</h4>    
             
         </div>
             
